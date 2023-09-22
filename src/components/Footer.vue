30708a9324e3e84fbe2302205943f11dc0aa46ae
Ahmad Hassan <muhammad.ahmadhassan@qbatch.com>
2023-09-22 12:10:00 +0500

    Footer Added

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template lang="">
+  <div class="w-full bg-white">
+    <p class="text-[#9299B8] text-">2023 © New Amazon Project</p>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,11 +2,14 @@
 <template>
   <div class="bg-[#F4F4F8] min-h-screen">
     <Header />
-    <div class="flex bg-[#F4F4F8]">
-      <SideBar style="flex: 0 0 220px" />
-      <div class="mx-[24px] flex-grow pt-[32px]">
-        <RouterView />
+    <div>
+      <div class="flex bg-[#F4F4F8] w-full">
+        <SideBar style="flex: 0 0 220px" />
+        <div class="mx-[24px] flex-grow pt-[32px]">
+          <RouterView />
+        </div>
       </div>
+      <Footer class="pl-[244px] py-[8px]" />
     </div>
   </div>
 </template>
@@ -15,5 +18,6 @@
 import { RouterView } from "vue-router";
 import Header from "../components/HeaderComponent.vue";
 import SideBar from "../components/SideBar.vue";
+import Footer from "../components/Footer.vue";
 </script>
 <style lang=""></style>
